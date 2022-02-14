2c22d455dc3ccbc7fdc9b58a14db6e8e5739b2a4
Dean <dean@deansantillan.com>
2022-02-14 14:35:08 +1000

    tasks(): added about, home and notfound pages and added them to app with routing. initialized github api key and set up environment variables.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,8 @@
 import NavBar from '../components/layout/navbar/NavBar.component';
 import Footer from '../components/layout/footer/Footer.component';
+import Home from '../pages/home/Home.page';
+import About from '../pages/about/About.page';
+import NotFound from '../pages/notfound/NotFound.page';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -7,10 +10,16 @@ function App() {
 		<Router>
 			<div className='flex flex-col justify-between h-screen'>
 				<NavBar />
-				<main className='container mx-auto px-3 pb-12'>Content</main>
-				<Footer/>
+				<main className='container mx-auto px-3 pb-12'>
+					<Routes>
+						<Route exact path='/' element={<Home />} />
+						<Route exact path='/about' element={<About />} />
+						<Route path='/notfound' element={<NotFound />} />
+						<Route path='/*' element={<NotFound />} />
+					</Routes>
+				</main>
+				<Footer />
 			</div>
-			<Routes></Routes>
 		</Router>
 	);
 }
---
+++ src/pages/home/Home.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+	return <div>
+        <h1 className="text-6xl">Welcome To GitHub SpoTTerfy.</h1>
+        {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
+    </div>;
+};
+export default Home;
